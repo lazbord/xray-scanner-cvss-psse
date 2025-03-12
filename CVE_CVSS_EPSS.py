00158e1429d0eb028973d9fa1722fad512a25ab0
Bordereaux <lbordereaux866@headmind.com>
2025-03-12 16:45:26 +0100

    sdfkodsvnjnvsf

--- CVE_CVSS_EPSS.py
+++ CVE_CVSS_EPSS.py
@@ -146,7 +146,7 @@ def funcNbCVEglobal():
     return nbCVEglobal
 
 nbCVEglobal = funcNbCVEglobal()
-nbCVEglobal = 10000
+nbCVEglobal = 765211
 CVE_CVSS_EPSS_table = []
 offsetGlobal = [2000,0] 
 nbReq = math.ceil(nbCVEglobal/ offsetGlobal[0])
